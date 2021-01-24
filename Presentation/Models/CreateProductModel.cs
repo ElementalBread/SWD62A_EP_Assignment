241b6e0e10f48cc939e20aef645fab81e5bae194
Mikel Mangion <mikel.mangion.e21422@mcast.edu.mt>
2021-01-24 01:52:11 +0100

    Model changes in Presentation layer. Removed conn string from base appsettings.json

--- Presentation/Models/CreateProductModel.cs
+++ Presentation/Models/CreateProductModel.cs
@@ -13,8 +13,6 @@ namespace Presentation.Models
 
     }
 
-
-
     public class ListProductModel
     {
         public List<CategoryViewModel> Categories { get; set; }
--- Presentation/Models/ErrorViewModel.cs
+++ Presentation/Models/ErrorViewModel.cs
@@ -1,11 +1,8 @@
 using System;
 
-namespace Presentation.Models
-{
-    public class ErrorViewModel
-    {
+namespace Presentation.Models{
+    public class ErrorViewModel{
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
